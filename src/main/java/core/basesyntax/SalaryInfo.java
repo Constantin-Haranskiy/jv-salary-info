e39d08d38ce7ed226e9a831acb20c8f6e575a54f
Con <con@MacBook-Air-Con.lan>
2026-05-30 23:21:22 +0200

    Fixed code style

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -16,8 +16,8 @@ public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy", Locale.ENGLISH);
-        LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
-        LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
+        LocalDate localFrom = LocalDate.parse(dateFrom, formatter);
+        LocalDate localTo = LocalDate.parse(dateTo, formatter);
         Map<String, Integer> salaryByName = new HashMap<>();
 
         StringBuilder result = new StringBuilder();
@@ -30,7 +30,7 @@ public class SalaryInfo {
             salaryByName.put(name, 0);
         }
 
-        if (!localDateTo.isBefore(localDateFrom)) {
+        if (!localTo.isBefore(localFrom)) {
             for (String record: data) {
                 if (record != null) {
                     String[] recordParts = record.split(" ");
@@ -41,8 +41,11 @@ public class SalaryInfo {
                         String recordHours = recordParts[HOURS_INDEX];
                         String recordIncome = recordParts[INCOME_INDEX];
 
-                        if (!recordDate.isBefore(localDateFrom) && !recordDate.isAfter(localDateTo)) {
-                            salaryByName.put(recordName, salaryByName.get(recordName) + Integer.parseInt(recordHours) * Integer.parseInt(recordIncome));
+                        if (!recordDate.isBefore(localFrom) && !recordDate.isAfter(localTo)) {
+                            Integer calculatedIncome = salaryByName.get(recordName)
+                                    + Integer.parseInt(recordHours)
+                                    * Integer.parseInt(recordIncome);
+                            salaryByName.put(recordName, calculatedIncome);
                         }
                     }
                 }
